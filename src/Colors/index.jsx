d4bd8e08ba9942f5e03de05de297e989047aaa75
LuxWise <jcsanchez.martinez.2020@example.com>
2023-12-02 00:03:08 -0500

    v1.1.0

--- src/Colors/index.jsx
+++ src/Colors/index.jsx
@@ -1,7 +1,27 @@
-export const ColorsDark = {
-  background: '#191a1f',
+import {useColorScheme} from 'react-native';
+
+export const backgroundStyle = {
+  backgroundColor: useColorScheme() === 'dark' ? '#191a1f' : '#f5f9fc',
+  backgroundColorBox: useColorScheme === 'dark' ? '#363dc6' : '#0066CC',
 };
 
-export const ColorsLight = {
-  background: '#f5f9fc',
+export const headerStyle = {
+  color: useColorScheme === 'dark' ? '#ffffff' : '#080808',
+};
+
+export const textStyle = {
+  color: useColorScheme === 'dark' ? '#9fa3b6' : '#080808',
+};
+
+export const shadowStyle = {
+  shadowColor: useColorScheme === 'dark' ? '#00000040' : '#000000',
+  shadowOffset:
+    useColorScheme === 'dark' ? {width: 0, height: 4} : {width: 0, height: 0},
+  shadowOpacity: useColorScheme === 'dark' ? 1 : 0.19,
+  shadowRadius: useColorScheme === 'dark' ? 20 : 5.62,
+  elevation: useColorScheme === 'dark' ? 8 : 6,
+};
+
+export const borderStyle = {
+  borderColor: useColorScheme === 'dark' ? '#191a1f' : '#f0f5ff',
 };
